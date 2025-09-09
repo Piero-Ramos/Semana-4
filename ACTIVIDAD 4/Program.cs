6fb070f8241d1200916e60eff7b567dd862adc60
Mathias-Salazar <mathiassl120106@gmail.com>
2025-09-09 10:27:33 -0500

    Código
    envio el codigo de la actividad

---
+++ ACTIVIDAD 4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadSemana4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pro, uni;
+            double precio, unid, subtotal;
+
+            Console.Write("Ingrese el precio del producto: ");
+            pro = Console.ReadLine();
+            precio = double.Parse(pro);
+            Console.WriteLine();
+            Console.Write("Ingrese la cantidad de unidades del producto: ");
+            uni = Console.ReadLine();
+            unid = double.Parse(uni);
+            Console.WriteLine();
+            subtotal = precio * unid;
+            Console.WriteLine("El subtotal es de: "+subtotal+"$");
+            Console.WriteLine();
+
+            if (subtotal > 100)
+            {
+                Console.WriteLine("Se le aplicará un descuento del 15%");
+                double descuento;
+                descuento = (subtotal * 5) / 100;
+                Console.WriteLine("Descuento total: "+descuento+"$");
+                Console.WriteLine();
+                double preciofinal;
+                preciofinal = subtotal - descuento;
+                Console.WriteLine("El precio final es de: "+preciofinal+"$");
+
+            }
+            else
+            {
+                {
+                    Console.WriteLine("El precio final es de:"+subtotal+"$");
+                }
+            }
+        }
+    }
+}
--- Ejemplo ppt5/Program.cs
+++ Ejemplo ppt5/Program.cs
@@ -10,18 +10,24 @@ namespace Ejemplo_ppt5
     {
         static void Main(string[] args)
         {
-            double a, b, c;
+            int NUM1;
             string linea;
-            Console.Write("Ingrese cateto 1: ");
+            long RESUL;
+            Console.Write("Digite un número :");
             linea = Console.ReadLine();
-            a = double.Parse(linea);
-            Console.Write("Ingrese cateto 2: ");
-            linea = Console.ReadLine();
-            b = double.Parse(linea);
-            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            NUM1 = int.Parse(linea);
+            RESUL = Math.Abs(NUM1);
             Console.WriteLine();
-            Console.Write("El valor de la hipotenusa es: {0}", c);
-            Console.ReadKey();
+            Console.WriteLine("Valor absoluto : " + RESUL);
+            Console.WriteLine("Potencia : " + Math.Pow(NUM1, 5));
+            Console.WriteLine("Raíz cuadrada : " + Math.Sqrt(NUM1));
+            Console.WriteLine("Núm. máximo : " + Math.Max(NUM1, 55));
+            Console.WriteLine("Núm. mínimo : " + Math.Min(NUM1, 55));
+            Console.WriteLine("Parte entera : " +
+            Math.Truncate(19.88));
+            Console.WriteLine("Redondeo : " + Math.Round(19.88));
+            Console.Write("Pulse una Tecla:");
+            Console.ReadLine();
         }
     }
 }
